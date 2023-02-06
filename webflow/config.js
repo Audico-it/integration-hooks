c87a795ef08070082c55a258efcfab0bfcd39286
Audico-it <marcus.paivio@audico.fi>
2023-02-06 15:36:26 +0200

    Update config.js

--- webflow/config.js
+++ webflow/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "", // weglot api_key
+  apiKey: "wg_e94109ba17ec7b2bb792670032dc02ad9", // weglot api_key
 
-  wflogin: "", // wflogin cookie content
-  wfsession: "", // wfsession cookie content
-  projectName: "", // url slug
+  wflogin: "1675659281428", // wflogin cookie content
+  wfsession: "Rfp18k_0nRJta-KzFJkASA.CG8-t3NyIr6w94R3adCU6Y_DFWSSjcwMkAPslW28Sr8LNEL3ZCBwctViUZoEcUl6LhHPfjnujUgOonMVLhdxPPuOPm23_gbROaoRspPI8aLB6bMY1v4fe8Qv39JPM3U_xuw0SMH4KVk9sFlMr2605iAMSYPjy98zYlG95YfKrRI.1676744028114.86400000.w1svZLvtiR2mRgjbLtwC5b2apYcZgy9H0AaYLPVfX0M", // wfsession cookie content
+  projectName: "audicosystems", // url slug
 
   overwrite: false, // append or overwrite custom <head> content, default to append
 };
